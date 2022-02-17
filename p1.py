46e207b0a08c89c30dff2b0ee27b1059c559a587
email <mmb2020max@gmail.com>
2022-02-17 21:35:08 +0200

    fontdic for axis Done

--- p1.py
+++ p1.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -9,7 +10,12 @@ y=[5,3,20,56]
 # plt.plot(x,y,'o-.')
 # plt.plot(x,y,'.-.')
 # plt.plot(x,y,'x-')
-plt.plot(x,y,'p-r')
+# plt.plot(x,y,'p-r') #p for polygon r for red 
+plt.plot(x,y,'p-r',ms=20,mec='b',mfc='g')
+font1={'family':'tahoma','color':'blue','size':20}
+
+plt.xlabel('x-axis',fontdict=font1)
+plt.ylabel('y-axis',fontdict=font1)
 plt.show()
 
 
