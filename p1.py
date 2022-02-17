39d957841d2c85c3ce935660e40144c0c1ad78a1
email <mmb2020max@gmail.com>
2022-02-17 21:10:25 +0200

    color Added

--- p1.py
+++ p1.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x=[1,8]
-y=[5,3]
+x=[1,8,13,45]
+y=[5,3,20,56]
 
-plt.plot(x,y,'o--')
+# plt.plot(x,y,'o--')
+# plt.plot(x,y,'o:')
+# plt.plot(x,y,'o-.')
+# plt.plot(x,y,'.-.')
+# plt.plot(x,y,'x-')
+plt.plot(x,y,'p-r')
 plt.show()
 
 
