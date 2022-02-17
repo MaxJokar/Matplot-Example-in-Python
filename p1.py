875740f375131184fa3212c21f55067cf1ee1e76
email <mmb2020max@gmail.com>
2022-02-17 21:42:27 +0200

    box and color for the box

--- p1.py
+++ p1.py
@@ -1,5 +1,6 @@
 
 import matplotlib.pyplot as plt
+from matplotlib.transforms import Bbox
 import numpy as np
 
 x=[1,8,13,45]
@@ -13,9 +14,12 @@ y=[5,3,20,56]
 # plt.plot(x,y,'p-r') #p for polygon r for red 
 plt.plot(x,y,'p-r',ms=20,mec='b',mfc='g')
 font1={'family':'tahoma','color':'blue','size':20}
-
+font2={'family':'tahoma','color':'green','size':15}
+font3={'family':'tahoma','color':'black','size':10}
+plt.title('My-Chart',fontdict=font2)
 plt.xlabel('x-axis',fontdict=font1)
 plt.ylabel('y-axis',fontdict=font1)
+plt.text(10,40,'MyText',fontdict=font3,bbox=dict(facecolor='red',alpha=0.5))
 plt.show()
 
 
